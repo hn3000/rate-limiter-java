823fedde6913bc511729a8497e7453307c85a7fb
Harald Niesche <harald@niesche.de>
2021-04-18 05:26:57 +0200

    improve interface documentation

--- src/main/java/de/niesche/commons/ratelimiter/IRateLimiter.java
+++ src/main/java/de/niesche/commons/ratelimiter/IRateLimiter.java
@@ -27,10 +27,28 @@ package de.niesche.commons.ratelimiter;
  *
  */
 public interface IRateLimiter {
+    /** Consumes 1.0 amount, returns false if that is not possible. */
 	boolean consumeAmount();
-	boolean consumeAmount(double numPermits);
-    long waitTimeForAmount();
-    long waitTimeForAmount(double numPermits);
+    /** Consumes amount, returns false if that is not possible. */
+	boolean consumeAmount(double amount);
+
+    /** 
+     * Gives time required to wait until amount 1.0 can be consumed.
+     * The call does not consume the amount and should not be used
+     * from multiple threads, since nothing prevents starvation of
+     * threads.
+     * Returns WAIT_IMPOSSIBLE if it will never allow the amount. 
+     */
+	long waitTimeForAmount();
+
+	/** 
+     * Gives time required to wait until amount can be consumed. 
+     * The call does not consume the amount and should not be used
+     * from multiple threads, since nothing prevents starvation of
+     * threads.
+     * Returns WAIT_IMPOSSIBLE if it will never allow the amount. 
+     */
+    long waitTimeForAmount(double amount);
     
 	public interface ITimeSource {
 	  long currentTimeMillis();
